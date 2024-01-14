6e6f0a558ef8c79514467a5db9e0291e28248911
hemanth <hemanth.hsm@gmail.com>
2024-01-14 20:04:24 +0530

    Changed made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hey, I am Hemanth Singh. This is the changes made and checking out the pipeline whether it is working fine.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
